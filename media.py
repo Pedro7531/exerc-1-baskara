1085c6e5e17631eea750303eccb58a0325955dbb
pedro7531 <pedrohenrique754@icloud.com>
2024-09-11 22:56:06 -0300

    (versao para calcular media)

---
+++ media.py
@@ -0,0 +1,8 @@
+num1 = float(input("Informe o valor 1:"))
+num2 = float(input("Informe o valor 2:"))
+num3 = float(input("Informe o valor 3:"))
+num4 = float(input("Informe o valor 4:"))
+
+media = (num1 + num2 + num3 + num4)/4
+
+print("média = ",media)
